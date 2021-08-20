26fe4008398de0f9e20d284ece3613dd0dfc9615
Jimmy Tan <jimmytan0424@gmail.com>
2021-08-19 21:57:31 -0700

    styling for about and fav pages

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,25 +1,32 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { appTitle } from '../globals/globalVariables';
-
+import tmdbLogo from '../images/tmdb-logo.svg';
 
 const PageAbout = () => {
   //change tab title when rendering
   useEffect(() => {
     document.title = `About - ${appTitle}`;
-}, []);
+  }, []);
 
   return (
-    <main className='about-main-section'>
-      <p>This is the about page</p>
+    <main className="about-main-section">
+      <h2>
+        About <span className='logo'><span className="logo-m">M</span>ovie
+        <span className="logo-w">W</span>ave</span>
+      </h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta officia
-        amet ipsam totam, ipsum neque voluptatibus! Maxime fugiat illum veniam,
-        distinctio mollitia ut. Adipisci nobis iusto nisi facilis possimus
-        tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
-        dolorem doloremque rerum minus exercitationem aut amet inventore,
-        officiis tempora dolor recusandae esse laudantium a quibusdam pariatur
-        quia minima cupiditate. Iusto?
+        Welcome to the MovieWave database. This is an online database where
+        movie lovers can find the relevant movie information based on popular,
+        top rated, now playing and upcoming movies. This application also let user add
+        and store movies to the favourite list.
       </p>
+      <p>
+        This application uses the TMDb API but is not endorsed or certified by
+        TMDb.
+      </p>
+      <div>
+        <img className="tmdb-logo" src={tmdbLogo} alt="TMDB Logo" />
+      </div>
     </main>
   );
 };
--- src/pages/PageFavourites.js
+++ src/pages/PageFavourites.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import useGlobal from '../store/globalAppState';
 import Movies from '../components/Movies';
 import { appTitle } from '../globals/globalVariables';
+import { unFilledHeartIcon } from '../globals/icon';
 
 const PageFavourites = () => {
   //change tab title when rendering
@@ -18,11 +18,13 @@ const PageFavourites = () => {
 
   return (
     <main className="favourites-main-section">
-      <h2>Favourites Movie</h2>
+      <h2>
+        <span className="orange-text">Favourites</span> Movie
+      </h2>
       {globalState.favs.length === 0 ? (
-        <p>
-          No favourited movies. Please visit the <Link to="/">homepage</Link> to
-          favourite a movie.
+        <p className='no-favs-text'>
+          No favourited movies. Please discover your favourites and click
+          {unFilledHeartIcon} to add some movies here.
         </p>
       ) : (
         <Movies movies={globalState.favs} checkFav={false} />
