229ba8a77c1448c74f967c5dc3e4fc77110ba50e
Jimmy Tan <jimmytan0424@gmail.com>
2021-12-13 22:59:23 -0800

    change minor styling and add scroll to top when route changes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 //Components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 //Pages
 import PageHome from '../pages/PageHome';
@@ -16,9 +17,7 @@ import Page404 from '../pages/Page404';
 
 import { APP_FOLDER_NAME } from '../globals/globalVariables';
 
-
 const AppRouter = () => {
-
   //state for light/dark mode theme for the application
   //default is set to dark mode, can toggle mode via the button in site header
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -28,13 +27,26 @@ const AppRouter = () => {
       <div className="site-wrapper">
         <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
         <main className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+          <ScrollToTop />
           <Switch>
-            <Route path='/' exact><PageHome /></Route>
-            <Route path='/about'><PageAbout /></Route>
-            <Route path='/favourites'><PageFavourites /></Route>
-            <Route path='/movie/:id'><PageSingleMovie /></Route>
-            <Route path='/search'><PageSearch /></Route>
-            <Route><Page404 /></Route>
+            <Route path="/" exact>
+              <PageHome />
+            </Route>
+            <Route path="/about">
+              <PageAbout />
+            </Route>
+            <Route path="/favourites">
+              <PageFavourites />
+            </Route>
+            <Route path="/movie/:id">
+              <PageSingleMovie />
+            </Route>
+            <Route path="/search">
+              <PageSearch />
+            </Route>
+            <Route>
+              <Page404 />
+            </Route>
           </Switch>
         </main>
         <Footer />
